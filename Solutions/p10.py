a1f51994d1ba071562b7515658761412df351afa
Jan <j.borchmann@gmx.de>
2018-11-15 19:29:04 -0500

    linting changes

--- Solutions/p10.py
+++ Solutions/p10.py
@@ -1,29 +1,34 @@
-n = 2000000
-sieve = [True] * n
+"""Solution for problem 10
+"""
+
+N = 2000000
+SIEVE = [True] * N
 
 def mark(sieve, x):
-    for i in range(x+x, len(sieve), x):
-        sieve[i] = False
+    """This function marks prime numbers
+    """
+    for number in range(x+x, len(sieve), x):
+        sieve[number] = False
 
-for x in range(2,int(len(sieve)**0.5)+1):
-    if sieve[x]:
-        mark(sieve,x)
+for x in range(2, int(N**0.5) + 1):
+    if SIEVE[x]:
+        mark(SIEVE, x)
 
 
 def test_prime(n):
-    if sieve[n]:
+    """Tests if a number is prime
+    """
+    if SIEVE[n]:
         return True
     else:
         return False
 
-def prime_euqation(n,a,b):
-    return n**2 + a*n +b
+def prime_euqation(n, a, b):
+    return n**2 + a * n + b
 
 summe = 0
-for i in range(2,2000000):
-    if sieve[i]:
+for i in range(2, 2000000):
+    if SIEVE[i]:
         summe += i
 
 print(summe)
-
-
