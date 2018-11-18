d88460ae15d34547105d6fd2925e46fa5ed716c8
Jan <j.borchmann@gmx.de>
2018-11-18 12:39:08 -0500

    linting changes to p185

--- Solutions/p185_genetic.py
+++ Solutions/p185_genetic.py
@@ -1,7 +1,17 @@
+"""Solution to problem 185 by using a genetic algorithm.
+
+After my first attempt of using Monte Carlo which didn't end up working in a reasonable amount of
+time, I moved on and used a genetic algorithm for solving this problem. This ended up working
+nicely.
+
+For each step, I keep the best half of each generation and then use the following mutation steps:
+With probablity M generate new digit or with probablity 1-M take the digit at the same position
+from a random member of the old generation.
+"""
 from random import randint as ri
 from random import random as r
 
-guesses = {"5616185650518293" : 2,
+GUESSES = {"5616185650518293" : 2,
            "3847439647293047" : 1,
            "5855462940810587" : 3,
            "9742855507068353" : 3,
@@ -27,33 +37,59 @@ guesses = {"5616185650518293" : 2,
 G = 32 #the size of each generation
 G_S = 16 #the size of the subset of each generation that get selected
 M = 0.06 #probability of mutation
-S = len(guesses.keys().__iter__().__next__()) #string size
+S = len(GUESSES.keys().__iter__().__next__()) #string size
 
-#creates a population of length G
-populate = lambda x=None: [''.join([str(ri(0,9)) for x in range(S)]) for x in range(G)]
-# calculates the fitness of the generation by checking how many guesses are fulfilled 
-fitness = lambda m: sum([sum([g[x]==m[x] and 1 or 0 for x in range(S)])==guesses[g] and 1 or 0 for g in guesses]) 
-# creates a new generation by taking the selected group of the previous generation, and with a probability of M creates a new number 
-# or with (1-M) takes a number from a random member of the old generation. This way the full size of the population is reinstated
-new_generation = lambda s: [''.join([r()<M and str(ri(0,9)) or s[ri(0,G_S-1)][k] for k in range(0,S)]) for x in range(G)]
+#creates a population of size population size
+def populate(length_of_strings, population_size):
+    """This function creates a population of strings for a given population size.
+    """
+    return [''.join([str(ri(0, 9)) for x in range(length_of_strings)]) for x in range(
+        population_size)]
 
-# pop = populate()
-# selected = sorted(pop,key=fitness, reverse=True)[:G_S]
-# print(selected)
-# new_gen = new_generation(selected)
-# print(sorted(new_gen,key=fitness, reverse=True))
+# calculates the fitness of the generation by checking how many guesses are fulfilled
+# disabling pylint error here, since I don't edit GUESSES and this is needed as default for
+# usage as key in sorted
+def fitness(input_generation, length_of_strings=S, guesses=GUESSES): #pylint: disable=W0102
+    """Calculate fitness
 
+    This function calculates the fitness of each generation by counting how many
+    guesses are fulfilled. It counts how many digits coincide with the known guesses. If that
+    number matches the correct number of digits, it counts 1 towards the overall fitness.
+    """
+    return sum([sum([g[x] == input_generation[x] and 1 or 0 for x in range(length_of_strings)]
+                   ) == guesses[g] and 1 or 0 for g in guesses])
+
+def new_generation(input_generation, length_of_strings, probability_of_mutation, population_size,
+                   subpopulation_size):
+    """Create new, mutated generation
+
+    This function creates a new generation by taking the selected group of the previous generation,
+    and with a probability of M creates a new number
+    or with (1-M) takes a number from a random member of the old generation.
+    This way the full size of the population is reinstated.
+    """
+    return [''.join(
+        [r() < probability_of_mutation and str(ri(0, 9)) #new number
+         or input_generation[ri(0, subpopulation_size - 1)][k] for k in range(0, length_of_strings)]
+        ) for x in range(population_size)]
 
 def main():
+    """main function that runs the algorithm
+    """
     high_score = 0
-    population = populate()
-    while high_score<len(guesses): 
-        selected = sorted(population,key=fitness, reverse=True)[:G_S]
-        gen_score = fitness(selected[0])
-        if gen_score>high_score: 
+    # initialize population
+    population = populate(S, G)
+    while high_score < len(GUESSES):
+        # select best G_S members of current population
+        selected = sorted(population, key=fitness, reverse=True)[:G_S]
+        # score the best members with respect to the guesses
+        gen_score = fitness(selected[0], S, GUESSES)
+        # if the current score is the new high score, update
+        if gen_score > high_score:
             high_score = gen_score
             print(high_score, selected[:5])
-        population = new_generation(selected)
+        # mutate population and generate new population
+        population = new_generation(selected, S, M, G, G_S)
 
 if __name__ == "__main__":
     main()
--- Solutions/p185_montecarlo.py
+++ Solutions/p185_montecarlo.py
@@ -1,87 +1,112 @@
-# in this approach I will use a stochastic approach that will try the most probable guess for each digit first and then move on to lower ones
-import numpy
-import time
+"""Monte Carlo approach to problem 185
+
+In this approach I will use a stochastic approach that will try the most probable guess for
+each digit first and then move on to lower ones. It turned out not to work in a reasonable amount
+of time, so I moved on to a genetic algorithm.
+"""
 import random
-from scipy.optimize import fmin_powell, minimize
+import time
+import numpy
 
 start = time.time()
 
 # input_dict = {90342: 2, 70794 : 0, 39458 : 2, 34109 : 1, 51545 : 2, 12531 : 1}
-# number_length = 5
+# NUMBER_LENGTH = 5
 
-input_dict = {5616185650518293 :2 ,3847439647293047 :1 ,5855462940810587 :3 ,9742855507068353 :3 ,4296849643607543 :3 ,3174248439465858 :1 ,4513559094146117 :2 ,7890971548908067 :3, 8157356344118483 :1 ,2615250744386899 :2 ,8690095851526254 :3 ,6375711915077050 :1 ,6913859173121360 :1 ,6442889055042768 :2 ,2321386104303845 :0 ,2326509471271448 :2 ,5251583379644322 :2 ,1748270476758276 :3 ,4895722652190306 :1 ,3041631117224635 :3 ,1841236454324589 :3 ,2659862637316867 :2 }
-number_length = 16
-#4640261571849533
+GUESSES = {"5616185650518293" : 2,
+           "3847439647293047" : 1,
+           "5855462940810587" : 3,
+           "9742855507068353" : 3,
+           "4296849643607543" : 3,
+           "3174248439465858" : 1,
+           "4513559094146117" : 2,
+           "7890971548908067" : 3,
+           "8157356344118483" : 1,
+           "2615250744386899" : 2,
+           "8690095851526254" : 3,
+           "6375711915077050" : 1,
+           "6913859173121360" : 1,
+           "6442889055042768" : 2,
+           "2321386104303845" : 0,
+           "2326509471271448" : 2,
+           "5251583379644322" : 2,
+           "1748270476758276" : 3,
+           "4895722652190306" : 1,
+           "3041631117224635" : 3,
+           "1841236454324589" : 3,
+           "2659862637316867" : 2}
+
+NUMBER_LENGTH = 16
 
 #this does not work!
 def check():
-    guesses = {i:[] for i in range(number_length)}
+    guesses = {i:[] for i in range(NUMBER_LENGTH)}
     for i in range(100):
         summe = 0
         liste = []
         while summe < 17:
-            number = random.choice(list(input_dict.keys()))
-            summe += input_dict[number]
+            number = random.choice(list(GUESSES.keys()))
+            summe += GUESSES[number]
             liste.append(number)
 
         if summe == 17:
-            for i in range(number_length):
+            for i in range(NUMBER_LENGTH):
                 ints = [str(number)[i] for number in liste]
 
-                guesses[i].extend(list( set([x for x in ints if ints.count(x) > 1])))
+                guesses[i].extend(list(set([x for x in ints if ints.count(x) > 1])))
 
 
-    guesses_set = {i:list(set(guesses[i])) for i in range(number_length)}
+    guesses_set = {i:list(set(guesses[i])) for i in range(NUMBER_LENGTH)}
 
-    for number in input_dict:
-        if input_dict[number] == 0:
-            for i,digit in enumerate(str(number)):
+    for number in GUESSES:
+        if GUESSES[number] == 0:
+            for i, digit in enumerate(str(number)):
 
                 guesses_set[i].remove(digit)
 
-    print([len(guesses_set[i]) for i in range(number_length)])
+    print([len(guesses_set[i]) for i in range(NUMBER_LENGTH)])
 
 
 # now we create the probability tables for each position
 def check_guess(n):
-    for number in input_dict.keys():
+    for number in GUESSES.keys():
         count = 0
         for i in range(len(str(n))):
             if str(n)[i] == str(number)[i]:
                 count += 1
 
-        if count != input_dict[number]:
+        if count != GUESSES[number]:
             return False
 
     return True
 
 def metric(n):
     distance = 0
-    for number in input_dict.keys():
+    for number in GUESSES.keys():
         count  = len([1 for i in range(len(str(n))) if str(n)[i] == str(number)[i] ])
-        distance += (count-input_dict[number])**2
+        distance += (count-GUESSES[number])**2
 
     return distance
 
 
 # the key is the position of the digit and the list will be filled with the probabilities for each number in order 0-9
-probability_dict = {i:[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1] for i in range(number_length)}
+probability_dict = {i:[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1] for i in range(NUMBER_LENGTH)}
 
 # if a number is ruled out, we set the probability to -1, in order to distinguish from the case where we have no information
-for number in input_dict.keys():
-    correct_count = input_dict[number]
+for number in GUESSES.keys():
+    correct_count = GUESSES[number]
     if correct_count > 0:
         for i,digit in enumerate(str(number)):
             if probability_dict[i][int(digit)] >= 0:
-                probability_dict[i][int(digit)] += correct_count/number_length
+                probability_dict[i][int(digit)] += correct_count/NUMBER_LENGTH
     else:
         for i,digit in enumerate(str(number)):
             probability_dict[i][int(digit)] = 0
 
 
-possibilities = {i:[0,1,2,3,4,5,6,7,8,9] for i in range(number_length)}
-for number in input_dict.keys():
-    correct_count = input_dict[number]
+possibilities = {i:[0,1,2,3,4,5,6,7,8,9] for i in range(NUMBER_LENGTH)}
+for number in GUESSES.keys():
+    correct_count = GUESSES[number]
     if correct_count == 0:
         for i,digit in enumerate(str(number)):
             possibilities[i][int(digit)] = 0
@@ -91,7 +116,7 @@ for number in input_dict.keys():
 
 def random_guess():
     guess = []
-    for i in range(number_length):
+    for i in range(NUMBER_LENGTH):
         len_poss = len(possibilities[i])
         rand = random.randint(0,len_poss-1)
         guess.append(str(possibilities[i][rand]))
@@ -99,12 +124,12 @@ def random_guess():
     return ''.join(guess)
 
 
-# norm_prob = {  i : numpy.ndarray.tolist(numpy.array(probability_dict[i])*1/sum(probability_dict[i])) for i in range(number_length)  }
+# norm_prob = {  i : numpy.ndarray.tolist(numpy.array(probability_dict[i])*1/sum(probability_dict[i])) for i in range(NUMBER_LENGTH)  }
 
 initial_guess = random_guess()
 old_metric = metric(int(initial_guess))
 number = list(initial_guess)
-print(initial_guess,old_metric)
+print(initial_guess, old_metric)
 
 len_poss = len(possibilities[0])
 
@@ -114,9 +139,9 @@ k = 1
 restarts = 0
 while True:
     #define random digit and copy number to temp
-    digit = random.randint(0,number_length-1)
+    digit = random.randint(0, NUMBER_LENGTH-1)
     temp_list = number[:] # I need to use the slice! If I use just temp_list = number it'll only create a reference!!!!
-    rand = random.randint(0,len_poss-1)
+    rand = random.randint(0, len_poss-1)
 
     #switch this digit in temp for the random one
     temp_list[digit] = str(possibilities[digit][rand])
@@ -127,7 +152,9 @@ while True:
         old_metric = new_metric
     elif old_metric - new_metric:
         T = 1/numpy.log((k+10)/10)
-        choice = numpy.random.choice(numpy.arange(0, 2), p=[1-numpy.exp(-(new_metric - old_metric)/T),numpy.exp(-(new_metric - old_metric)/T)])
+        choice = numpy.random.choice(numpy.arange(0, 2),
+                                     p=[1-numpy.exp(-(new_metric - old_metric)/T),
+                                        numpy.exp(-(new_metric - old_metric)/T)])
         if choice == 1:
             number = list(''.join(temp_list))
             old_metric = new_metric
@@ -148,7 +175,7 @@ while True:
             if restarts % 20 == 0:
                 print('Restarts: ', restarts)
             k = 1
-    k +=1
+    k += 1
 
 # maybe it would be cool to accept non-optimal solutions with a non-vanishing probability along the lines of exp(-(new_metric - old_metric)/T) for some T.
 # T could slowly decrease and thus "freeze out" the algorithm
@@ -156,7 +183,7 @@ while True:
 
 # #check most probable manually
 # number = ''
-# for i in range(number_length):
+# for i in range(NUMBER_LENGTH):
 #     digit = probability_dict[i].index(max(probability_dict[i]))
 #     number = number + str(digit)
 
@@ -167,7 +194,7 @@ while True:
 # else:
 #     while status:
 #         new_number = ''
-#         for i in range(number_length):
+#         for i in range(NUMBER_LENGTH):
 #             new_number += str(numpy.random.choice(numpy.arange(0, 10), p=norm_prob[i]) )
 #         if check_guess(int(new_number)):
 #             print('The number is %s' % new_number)
@@ -175,10 +202,3 @@ while True:
 
 
 print('The calculation took %s s.' % (time.time()-start))
-
-
-
-
-
-
-
