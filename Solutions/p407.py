4683ec62ec2f390da769ec6040e0490540612bed
Jan <j.borchmann@gmx.de>
2018-11-21 20:48:57 -0500

    added problem 407

---
+++ Solutions/p407.py
@@ -0,0 +1,74 @@
+"""Solution to problem 407
+
+M(n) = max_{a < n}(a^2 % n = a)
+
+M(2) = 1
+M(3) = 1
+M(4) = 1
+M(5) = 1
+M(6) = 4
+M(7) = 1
+M(8) = 1
+M(9) = 1
+M(10) = 6
+
+Theorem 1: for prime numbers n: M(n) = 1 (easy proof)
+Theorem 2: if M(n) = x and x > 1: gcd(n, x) > 1
+Theorem 1 follows from 2, since for any prime gcd(n, m) = 1 for all m
+"""
+from collections import defaultdict
+from functools import reduce
+from itertools import combinations
+from operator import mul
+from numba import jit
+
+@jit
+def factorize(n: int):
+    """This function factorizes all integers >= n and computes the totient.
+
+    It then returns the factors and a list of each number's value of Euler's totient
+    function (see project euler problem 69).
+    """
+    totient = list(range(n+1))
+    factors = defaultdict(list)
+    for number in range(2, n+1):
+        if number not in factors:
+            totient[number] = number -1
+            for i in range(2*number, n+1, number):
+                j, k = i, 1
+                while j % number == 0:
+                    j //= number
+                    k *= number
+                factors[i].append(k)
+                totient[i] = totient[i] * (number - 1) // number
+    return factors, totient
+
+def uw(f: dict, t:list , i: int):
+    """Helper function
+    """
+    for j in range(1, len(f[i])):
+        for c in combinations(f[i], j):
+            u = reduce(mul, c)
+            v = i // u
+            w = pow(u, t[v] - 1, v)
+            yield u * w
+
+@jit
+def main():
+    """main function
+    """
+    n = 10**7
+    f, t = factorize(n + 1)
+    summe = 0
+    for i in range(2, n + 1):
+        # in case i is prime
+        if i not in f or len(f[i]) < 2:
+            summe += 1
+            continue
+
+        summe += max(uw(f, t, i))
+
+    print(summe)
+
+if __name__ == "__main__":
+    main()
--- Solutions/p69.py
+++ Solutions/p69.py
@@ -1,31 +1,51 @@
+"""Solution to Project Euler problem 69
+"""
+
 def prime_factors(n):
+    """Calculates the prime factors of n and returns them in a list
+    """
     i = 2
     factors = []
     while i * i <= n:
+        # if i does not divide n, increase i by 1
         if n % i:
             i += 1
+        # if i divides n, divide n by i and append i to the factors
         else:
             n //= i
             factors.append(i)
+    # if we are done and the remainder of n is still larger 1, it is a prime and a divisor
     if n > 1:
         factors.append(n)
     return factors
 
 def phi(n):
+    """Computes Euler's totient function via
+
+    phi(n) = n * prod_i (1 - 1/p_i),
+
+    where p_i are the prime factors of n.
+    """
     prime_facts = prime_factors(n)
     result = n
-    for p in set(prime_facts):
-        result *= (1-1/p)
+    for prime in set(prime_facts):
+        result *= (1-1/prime)
     return result
 
-maximum_n = 0
-maximum = 0
-for n in range(2,1000000):
-    if n % 100000 == 0:
-        print(n)
-    val = n/phi(n)
-    if val > maximum:
-        maximum = val
-        maximum_n = n
+def main():
+    """Main function
+    """
+    maximum_n = 0
+    maximum = 0
+    for n in range(2, 1000000):
+        if n % 100000 == 0:
+            print(n)
+        val = n/phi(n)
+        if val > maximum:
+            maximum = val
+            maximum_n = n
 
-print(maximum_n, maximum)
+    print(maximum_n, maximum)
+
+if __name__ == "__main__":
+    main()
