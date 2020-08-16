34a19834e2f886c7a38d3ebc0300e2e8abb65423
Jan <j.borchmann@gmx.de>
2020-08-16 19:47:40 -0400

    added explanation

--- Solutions/101-200/p188.py
+++ Solutions/101-200/p188.py
@@ -5,7 +5,10 @@ However, a question that remains is: If we calculate an exponent of a number mod
 guarantee that we can calculate the next exponentiation also modulo m without losing some numbers.
 
 The answer to that question is, at least in this case, yes (since my solution works), but I am not
-fully sure I understand why this works.
+fully sure I understand why this works. I think it's because 1777 and 10^8 are coprime.
+
+One can actually derive x^(10^n) = 1 mod 10^(n+1). This means that the last n+1 digits of a^b are
+determined by the last n digits of b.
 """
 
 def T_mod(a, b, m):
